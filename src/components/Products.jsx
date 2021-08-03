cd5603053f7654cd3b61810ce132ea7ba2e5f6b0
Cullen Sharp <cullen.sharp100@gmail.com>
2021-08-02 19:39:28 -0700

    Style with material

---
+++ src/components/Products.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
+function Products () {
+  const products = useSelector(state => state.categories.products);
+
+  console.log(products);
+  return (
+    <React.Fragment>
+      {products.map(product => (
+        <Card key={product.name}>
+          <CardActionArea>
+            <CardMedia 
+              image="http://placekitten.com/200/300"
+              title="image of product"
+            />
+            <CardContent>
+              <Typography variant="h5" component="h2">
+                {product.name}
+              </Typography>
+              <Typography variant="body2" color= "textSecondary" component="p">
+                {product.description}
+              </Typography>
+            </CardContent>
+            <Button color="inherit">
+              Add to Cart
+            </Button>
+            <Button color="inherit">
+              View Details
+            </Button>
+          </CardActionArea>
+        </Card>
+      ))
+      }
+    </React.Fragment>
+  );
+}
+
+export default Products;
