774f5e32f75259806f41d93d091058f9049b1e5a
Cullen Sharp <cullen.sharp100@gmail.com>
2021-08-02 19:38:07 -0700

    Add Material components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 
+import Container from '@material-ui/core/Container';
+
 import './App.scss';
 
-import Header from './components/header';
+import Header from './components/Header';
+import Products from './components/Products'
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
+    <Container className="App">
       <Header />
-    </div>
+      <Products/>
+      <Footer />
+    </Container>
   );
 }
 
