b4845dd0f98bebc2eb0b2c0bd881f19adbc483ea
Cullen Sharp <cullen.sharp100@gmail.com>
2021-08-02 18:11:26 -0700

    Write v1 reducer

---
+++ store/reducers/categories.js
@@ -0,0 +1,17 @@
+const initialState = {
+  activeCategory: '',
+  categories: ['electronics', 'books'],
+  products: []
+}
+
+function categories(state = initialState, action) {
+  switch (action.type) {
+    case 'SET_ACTIVE_CATEGORY':
+      state.activeCategory = action.payload
+      break;
+    default:
+      return initialState
+  }
+}
+
+export default categories;
