88d9ada7a81e4d3c91062e5a773bd83869541192
Cullen Sharp <cullen.sharp100@gmail.com>
2021-08-02 18:37:51 -0700

    Convert to jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.scss';
+
+import Header from './components/header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
