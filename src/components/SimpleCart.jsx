03154a0afc7868aa0472f837665f27f8afd4ec3f
Cullen Sharp <cullen.sharp100@gmail.com>
2021-08-03 17:54:16 -0700

    #7 Increment/decrement quantity of items in cart

--- src/components/SimpleCart.jsx
+++ src/components/SimpleCart.jsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { removeItemFromCart } from '../store/reducers/categories';
+import { removeItemFromCart, changeQuantity} from '../store/reducers/categories';
 
 import Box from '@material-ui/core/Box';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import Typography from '@material-ui/core/Typography';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 import CloseIcon from '@material-ui/icons/Close';
 
 
@@ -18,13 +20,27 @@ function SimpleCart() {
 
   useEffect(() => {
     setProducts([...cartItems])
-  }, [cartItems])
+  }, [cartItems]);
 
   return (
     <Box>
       <List>
         { products.map( product => (
           <ListItem key={product.name}>
+            <Select
+              value={cartItems
+                  .find(item => item.name === product.name).quantity}
+              onChange={(e) => dispatch(
+                changeQuantity({name: product.name, quantity: e.target.value})
+              )}
+              displayEmpty
+            >
+              <MenuItem value={1}> 1 </MenuItem>
+              <MenuItem value={2}> 2 </MenuItem>
+              <MenuItem value={3}> 3 </MenuItem>
+              <MenuItem value={4}> 4 </MenuItem>
+              <MenuItem value={5}> 5 </MenuItem>
+            </Select>
             <Typography variant="body2">
               {product.name}
             </Typography>
--- src/store/reducers/categories.js
+++ src/store/reducers/categories.js
@@ -21,18 +21,21 @@ initialState.products.push({
   name: 'TV',
   category: 'electronics',
   description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque atque, facilis, quod deleniti assumenda amet autem quibusdam quidem molestiae at unde perferendis fugiat molestias nemo, obcaecati hic sequi facere debitis quia earum. Asperiores eaque, accusamus sapiente consectetur alias porro ratione necessitatibus aliquid aliquam natus tempora expedita ea quo velit placeat quisquam provident eum. Voluptatibus, ratione. Soluta exercitationem asperiores quisquam quos nemo nihil quo! Blanditiis obcaecati dolores magni quis voluptate culpa assumenda beatae at odit. Accusamus deleniti at dignissimos facere sed atque doloremque dolore incidunt id mollitia eveniet, itaque eos debitis soluta praesentium laborum nostrum voluptates maxime fugit! Nam, non. Commodi?',
+  quantity: 1,
 })
 
 initialState.products.push({
   name: 'Mac',
   category: 'electronics',
   description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque atque, facilis, quod deleniti assumenda amet autem quibusdam quidem molestiae at unde perferendis fugiat molestias nemo, obcaecati hic sequi facere debitis quia earum. Asperiores eaque, accusamus sapiente consectetur alias porro ratione necessitatibus aliquid aliquam natus tempora expedita ea quo velit placeat quisquam provident eum. Voluptatibus, ratione. Soluta exercitationem asperiores quisquam quos nemo nihil quo! Blanditiis obcaecati dolores magni quis voluptate culpa assumenda beatae at odit. Accusamus deleniti at dignissimos facere sed atque doloremque dolore incidunt id mollitia eveniet, itaque eos debitis soluta praesentium laborum nostrum voluptates maxime fugit! Nam, non. Commodi?',
+  quantity: 1,
 })
 
 initialState.products.push({
   name: 'Crying of the Latke 49',
   category: 'books',
   description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque atque, facilis, quod deleniti assumenda amet autem quibusdam quidem molestiae at unde perferendis fugiat molestias nemo, obcaecati hic sequi facere debitis quia earum. Asperiores eaque, accusamus sapiente consectetur alias porro ratione necessitatibus aliquid aliquam natus tempora expedita ea quo velit placeat quisquam provident eum. Voluptatibus, ratione. Soluta exercitationem asperiores quisquam quos nemo nihil quo! Blanditiis obcaecati dolores magni quis voluptate culpa assumenda beatae at odit. Accusamus deleniti at dignissimos facere sed atque doloremque dolore incidunt id mollitia eveniet, itaque eos debitis soluta praesentium laborum nostrum voluptates maxime fugit! Nam, non. Commodi?',
+  quantity: 1,
 })
 
 export function setActiveCategory(category) {
@@ -56,6 +59,13 @@ export function removeItemFromCart(name) {
   }
 }
 
+export function changeQuantity({name, quantity}) {
+  return {
+    type: 'CHANGED_QUANTITY_OF_ITEMS_IN_CART',
+    payload: {name, quantity}
+  }
+}
+
 function categories(state = initialState, action) {
   switch (action.type) {
     case 'SET_ACTIVE_CATEGORY':
@@ -63,15 +73,39 @@ function categories(state = initialState, action) {
         {...state, activeCategory: action.payload}
       );
     case 'ADDED_ITEM_TO_CART':
-      return (
-        {...state, cart: [...state.cart, action.payload]}
-      );
+      const index = state.cart.indexOf(action.payload);
+      if (index !== -1) {
+        const { cart } = state;
+        cart[index].quantity += 1;
+
+        return (
+          {...state, cart: [...cart]}
+        );
+      } else {
+        return (
+          {...state, cart: [...state.cart, action.payload]}
+        );
+      }
     case 'REMOVED_ITEM_FROM_CART':
       const filteredCart = state.cart
           .filter( product => product.name !== action.payload);
       return (
         {...state, cart: [...filteredCart]}
       );
+    case 'CHANGED_QUANTITY_OF_ITEMS_IN_CART':
+        const product = state
+            .cart
+            .find(product => product.name === action.payload.name);
+        
+        product.quantity = action.payload.quantity;
+
+        // todo find a better name for this
+        const removedItem = state.cart
+            .filter( product => product.name !== action.payload.name);
+
+        return(
+          {...state, cart: [...removedItem, product]}
+        ) 
     default:
       return initialState
   }
