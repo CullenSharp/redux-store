552c491d53c682166e86208b57dc782075204379
Cullen Sharp <cullen.sharp100@gmail.com>
2021-08-05 15:17:08 -0700

    #19 Organize components

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -8,7 +8,7 @@ import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 
-import { addItemToCart, fetchProducts } from '../store/actions/actions'
+import { addItemToCart, fetchProducts } from '../../store/actions/actions'
 
 function Products () {
   const [products, setProducts] = useState([]);
